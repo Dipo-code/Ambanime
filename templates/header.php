a54de8d27969002dcc73114a95906dabeaa506f5
Dipo Letris <dipoarya420@gmail.com>
2025-05-25 19:58:54 +0700

    menambahkan navbar pada header.php

---
+++ templates/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../public/assets/css/main.css">
+  <title>Ambanime</title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+<body class="bg-gray-200 font-sans">
+<!-- Navbar -->
+<nav class="bg-cyan-800 text-white px-6 py-3  ">
+  <div class="max-w-9xl mx-auto flex justify-between items-center">
+
+    <div class="text-2xl font-bold  ">AMBANIME</div>
+    <ul class="flex space-x-8 text-lg">
+      <li><a href="#" class="hover:underline">Home</a></li>
+      <li><a href="#" class="hover:underline">Daftar Anime</a></li>
+      <li><a href="#" class="hover:underline">Jadwal Rilis</a></li>
+    </ul>
+    <div class=""><input class="bg-gray-100 text-cyan-800   rounded-sm  px-3 py-1 focus:outline-none" type="text" placeholder="Cari anime disini..."></div>
+  </div>
+</nav>
+
+<!-- end navbar -->
+</body>
+</html>
