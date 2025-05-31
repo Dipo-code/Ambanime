1c67444239b85d61181031f3af936643ced8ea52
momoyoyomomo <noverefridho1@gmail.com>
2025-06-01 00:09:14 +0700

    Menambah Daftar Anime

--- public/daftar-anime.php
+++ public/daftar-anime.php
@@ -1,30 +1,45 @@
 <?php
 include '../templates/header.php';
 ?>
-<section class="container mx-auto ">
+<section>
+    <div class="container mx-auto py-10">
+        <h1 class = "font text-4xl font-semibold" >Genre</h1>
+        <div class="grid grid-cols-5 gap-y-10 mt-10">
+        <a href="" class="text-lg font-semibold">Action</a>
+        <a href="" class="text-lg font-semibold">Horor</a>
+        <a href="" class="text-lg font-semibold">Shonen</a>
+        <a href="" class="text-lg font-semibold">Comedy</a>
+        <a href="" class="text-lg font-semibold">Jomok</a>
+        <a href="" class="text-lg font-semibold">Yes king</a>
+        <a href="" class="text-lg font-semibold">Kebersamaan</a>
 
-    <div class="text-2xl font-bold mt-10 font-montserrat">Genre</div>
-    <ul class="mt-10 mb-5 flex space-x-8 text-lg">
-      <li><a href="index.php" class="hover:underline">Home</a></li>
-      <li><a href="daftar-anime.php" class="hover:underline">Daftar Anime</a></li>
-      <li><a href="jadwal-rilis.php" class="hover:underline">Jadwal Rilis</a></li>
-    </ul>
 
-    <div class="text-2xl font-bold mt-10 font-montserrat">Status</div>
-    <ul class="mt-10 mb-5 flex space-x-8 text-lg">
-      <li><a href="index.php" class="hover:underline">All</a></li>
-      <li><a href="daftar-anime.php" class="hover:underline">On-Going</a></li>
-      <li><a href="jadwal-rilis.php" class="hover:underline">Finished</a></li>
-    </ul>
-    <div class ="grid grid-cols-5 mt-20 gap-20 ">
-    <a href="#">
-    <div class=" w-64 h-80 relative rounded">
+        
+        </div>
+        <h1 class = "font text-4xl font-semibold mt-10" >Status</h1>
+        <div class="grid grid-cols-5 gap-y-5 mt-10">
+        <a href="" class="text-lg font-semibold">All</a>
+        <a href="" class="text-lg font-semibold">On-Going</a>
+        <a href="" class="text-lg font-semibold">Finished</a>
+
+
+        
+        </div>
+    </div>
+</section>
+<section class="bg-gray-900">
+    <div class="container mx-auto py-10">
+
+    <div class ="grid grid-cols-5 mt-20 gap-10 ">
+
+  <a href="#">
+    <div class=" w-60 h-80 relative rounded">
     <div class="absolute inset-0 bg-cover bg-center" style="background-image: url(assets/images/1283351.jpg);"></div>
     <!-- Layer gradasi hitam supaya teks lebih terlihat -->
     <div class="absolute bottom-0 left-0 right-0 h-20 bg-gradient-to-t bg-[linear-gradient(to_top,_#1F2937_20%,_transparent_150%)] to-transparent"></div>
     <!-- Judul card -->
     </div>
-    <div class="w-64 bg-gray-800 p-3 ">
+    <div class="w-60 bg-gray-800 p-3 ">
       
     <h2 class = "text-white text-lg font-semibold pb-3">Jojo  Batu Laut</h2>
     <div class="flex justify-between items-center">
@@ -33,9 +48,21 @@ include '../templates/header.php';
     </div>
     </div>
   </a> 
-
-
-</section>
+  <a href="#">
+    <div class=" w-60 h-80 relative rounded">
+    <div class="absolute inset-0 bg-cover bg-center" style="background-image: url(assets/images/1283351.jpg);"></div>
+    <!-- Layer gradasi hitam supaya teks lebih terlihat -->
+    <div class="absolute bottom-0 left-0 right-0 h-20 bg-gradient-to-t bg-[linear-gradient(to_top,_#1F2937_20%,_transparent_150%)] to-transparent"></div>
+    <!-- Judul card -->
+    </div>
+    <div class="w-60 bg-gray-800 p-3 ">
+      
+    <h2 class = "text-white text-lg font-semibold pb-3">Jojo  Batu Laut</h2>
+    <div class="flex justify-between items-center">
+      <h3 class = "text-white text-sm" >Episode 24</h3>
+      <h3 class = "text-white text-sm " >24 Menit/Eps </h3>
+    </div>
+    </div>
 <?php
 include '../templates/footer.php';
 ?>
